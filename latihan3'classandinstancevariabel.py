a5787acc4f698e7b4f5fd783bd98cf54083f5e29
Khifransyah <96522082+khifrvn@users.noreply.github.com>
2022-05-14 20:18:05 +0700

    Update latihan3'classandinstancevariabel.py

--- latihan3'classandinstancevariabel.py
+++ latihan3'classandinstancevariabel.py
@@ -10,7 +10,7 @@ class Hero:  # TEMPLATE
         self.defence = defence
         Hero.jumlah += 1
 
-# MAKE HERO
+# MAKE HERO COME TRUE
 hero1 = Hero('Sniper', 100, 500, 50)    # OBJECT / INSTANCE
 hero2 = Hero('Sven', 500, 250, 100)
 hero3 = Hero('Mirana', 150, 400, 25)
